4c06aeef7de571cb043c21344aa7b04169d82b2b
federico-calandra <57646675+federico-calandra@users.noreply.github.com>
2021-12-11 12:36:42 +0100

    Update 1-install.sh

--- scripts/1-install.sh
+++ scripts/1-install.sh
@@ -7,36 +7,38 @@
 # L'hard disk deve essere stato partizionato. La partizione formattata in ext4 e montata in /mnt
 # mount /dev/sdXy /mnt; mkswap /dev/swap_part; swapon /dev/swap_part; mkfs.ext4 /dev/sdXy
 
-read -p "imposta ororlogio"
+  read -p "Imposta layout tastiera"
+
+read -p "Imposta ororlogio"
 timedatectl set-timezone Europe/Rome; timedatectl set-ntp true
 
-read -p "installa pacchetti di base"
+read -p "Installa pacchetti di base"
 pacstrap /mnt linux linux-firmware amd-ucode base base-devel grub sudo zsh
 pacstrap  /mnt efibootmgr
 
-read -p "installa pacchetti di tools essenziali"
+read -p "Installa pacchetti di tools essenziali"
 pacstrap /mnt cpupower thermald turbostat
 pacstrap /mnt ntfs-3g usbutils nano wget android-udev os-prober git
 
-read -p "installa pacchetti networking"
+read -p "Installa pacchetti networking"
 pacstrap /mnt inetutils openssh x11vnc networkmanager nm-connection-editor network-manager-applet
 
-read -p "installa pacchetti desktop"
+read -p "Installa pacchetti desktop"
 pacstrap /mnt xorg xorg-xinit openbox tint2 gmrun rxvt-unicode redshift
 # pacstrap /mnt thunar thunar-volman thunar-media-tags-plugin
 
-read -p "genera fstab"
+read -p "Genera fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 nano /mnt/etc/fstab; wait
 
-read -p "genera locale"
+read -p "Genera locale"
 nano /etc/locale.gen; wait; locale-gen
 touch /mnt/etc/locale.conf /mnt/etc/vconsole.conf
 echo LANG=it_IT.UTF-8 >> /mnt/etc/locale.conf
 echo KEYMAP=it >> /mnt/etc/vconsole.conf
 
-read -p 'entra chroot'
+read -p 'Entra chroot'
 arch-chroot /mnt; wait
 
-read -p 'reboot'
+read -p 'Reboot'
 reboot
